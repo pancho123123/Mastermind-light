33042af0e3c5e36240a6553e4168dd75aa0e0324
pancho123123 <154936494+pancho123123@users.noreply.github.com>
2024-08-02 21:47:19 -0400

    Add files via upload

--- mastermind.py
+++ mastermind.py
@@ -14,30 +14,41 @@ colores.append(number_b)
 colores.append(number_c)
 colores.append(number_d)
 
+colores2 = colores
+
 colores_escogidos = []
+colores_escogidos2 = []
 value = []
 number_times = 4
 
 
 while colores != colores_escogidos:
     while len(colores_escogidos) < 4:
-        eleccion = input("Elige un color: rojo(r), verde(v), azul(a), morado(m), yellow(y), celeste(c), fucsia(f) o brown(b)")
+        eleccion = input("Elige color: rojo(r), verde(v), azul(a), morado(m), yellow(y), celeste(c), fucsia(f) o brown(b)")
         if eleccion == "r":
             colores_escogidos.append("rojo")
+            colores_escogidos2.append("rojo")
         elif eleccion == "v":
             colores_escogidos.append("verde")
+            colores_escogidos2.append("verde")
         elif eleccion == "a":
             colores_escogidos.append("azul")
+            colores_escogidos2.append("azul")
         elif eleccion == "m":
             colores_escogidos.append("morado")
+            colores_escogidos2.append("morado")
         elif eleccion == "y":
             colores_escogidos.append("yellow")
+            colores_escogidos2.append("yellow")
         elif eleccion == "c":
             colores_escogidos.append("celeste")
+            colores_escogidos2.append("celeste")
         elif eleccion == "f":
             colores_escogidos.append("fucsia")
+            colores_escogidos2.append("fucsia")
         elif eleccion == "b":
             colores_escogidos.append("brown")
+            colores_escogidos2.append("brown")
         else:
             pass
 
@@ -56,14 +67,16 @@ while colores != colores_escogidos:
             value.append("white")
             colores.remove(color)
 
+    print(colores_escogidos2)
     print(value)
     colores = []
     colores.append(number_a)
     colores.append(number_b)
     colores.append(number_c)
     colores.append(number_d)
-    print(colores)
+    #print(colores)
     colores_escogidos = []
+    colores_escogidos2 = []
     value = []
 
 
